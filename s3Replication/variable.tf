d44ae61d5a7e4272675e99231db1f55745da6887
bibek469 <47882764+bibek469@users.noreply.github.com>
2020-10-11 21:16:23 +0545

    Update variable.tf

--- s3Replication/variable.tf
+++ s3Replication/variable.tf
@@ -1,49 +1,49 @@
 variable "profile" {
   description = "Name the profile for setting up IAM group"
-  default     = "cc-sandbox"
+  default     = ""
 }
 
 variable "region" {
   description = "Name the region for setting up IAM group"
-  default     = "ap-southeast-1"
+  default     = ""
 }
 
 variable "id" {
   description = "Name the role ID"
-  default     = "ap-southeast-1"
+  default     = ""
 }
 
 variable "src_bucket_name" {
   description = "Name the source bucket"
-  default     = "cross-acc-share-gunj-db"
+  default     = "cross-example-bkt"
 }
 
 variable "src_bucket_arn" {
   description = "Name the source bucket"
-  default     = "arn:aws:s3:::cross-acc-share-gunj-db"
+  default     = ""
 }
 
 variable "src_region" {
   description = "Name the region for source bucket"
-  default     = "ap-southeast-1"
+  default     = ""
 }
 
 variable "dst_bucket_name" {
   description = "Name the destination bucket"
-  default     = "replication-bio-terra"
+  default     = ""
 }
 
 variable "dst_bucket_arn" {
   description = "Name the destination bucket"
-  default     = "arn:aws:s3:::s3-replication-bio-terra"
+  default     = ""
 }
 
 variable "dst_region" {
   description = "Name the region for destination bucket"
-  default     = "ap-south-1"
+  default     = ""
 }
 
 variable "role_arn" {
   description = "Name the region for setting up IAM group"
-  default     = "arn:aws:iam::626505363333:role/S3-Replication"
+  default     = ""
 }
